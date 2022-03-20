2c5593117338feaba5ae33c6009034351b6c649d
Jhonkar Sufia <windyforest13@gmail.com>
2022-03-19 21:21:19 -0400

    transition from monedas to comisiones ready. Follows transition from comisiones to monedas, and adjusting de vertical size of the viewport for horizontal view of the phone (must at least fit content)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+let exchangeRates      = document.getElementById( "exchangeRates"      );
+let exchangeRatesTitle = document.getElementById( "exchangeRatesTitle" );
 let monedas            = document.getElementById( "exchangeMonedas"    );
 let comisiones         = document.getElementById( "exchangeComisiones" );
 let exchangeRightArrow = document.getElementById( "exchangeRightArrow" );
 let exchangeLefttArrow = document.getElementById( "exchangeLeftArrow"  );
 
 const showComisiones = () => {
-    monedas.style.visibility    = "hidden";
+    monedas.style.opacity       = "0";
+    monedas.style.transform     = "translateX(-30vw)"
     comisiones.style.display    = "flex";
-    monedas.style.display       = "none";
-    comisiones.style.visibility = "visible";
+    monedas.style.position      = "absolute";
+    monedas.style.zIndex        = "1";
+    comisiones.style.zIndex     = "2";
+    ( window.innerHeight < 700 ) 
+        ? exchangeRatesTitle.scrollIntoView()
+        : exchangeRates.scrollIntoView();
+    setTimeout(
+        () => {
+            comisiones.style.transform  = "none";
+            comisiones.style.opacity    = "1";
+        }
+        , 0
+    )
 }
 
 const showMonedas = () => {
